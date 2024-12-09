624f17168953e2c95b762274e80357c59f545984
Shah Md. Mahi <shahmdmahi24@gmail.com>
2024-12-09 17:45:59 +0600

    updates

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import auth from "./actions/auth";
+import auth from "@/lib/auth";
 
 export async function middleware(request: Request) {
   const user = await auth.getUser();
