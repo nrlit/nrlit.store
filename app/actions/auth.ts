17f9612ab7a56d1c49b2fb7b16f69b33a57ea584
Shah Md. Mahi <shahmdmahi24@gmail.com>
2024-12-09 18:56:02 +0600

    updates

--- app/actions/auth.ts
+++ app/actions/auth.ts
@@ -2,20 +2,20 @@
 "use server";
 
 import auth from "@/lib/auth";
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 
 export async function loginAction(prevState: any, formData: FormData) {
   const result = await auth.createSession(formData);
-  if (result.success) {
-    redirect("/");
-  }
+  // if (result.success) {
+  //   redirect("/");
+  // }
   return result;
 }
 
 export async function registerAction(prevState: any, formData: FormData) {
   const result = await auth.createUser(formData);
-  if (result.success) {
-    redirect("/");
-  }
+  // if (result.success) {
+  //   redirect("/");
+  // }
   return result;
 }
