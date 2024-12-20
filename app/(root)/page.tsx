b36b48dec5c8bf18cd35a7ef21eeb1e8c3a0b0c5
Shah Md. Mahi <shahmdmahi24@gmail.com>
2024-12-21 03:26:40 +0600

    updates

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,18 @@
+import { CategoryGrid } from "@/components/category-grid";
+import { CategoryProducts } from "@/components/category-products";
+import { FeaturedProducts } from "@/components/featured-products";
 import { Hero } from "@/components/hero";
 
 export default async function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Hero />
+
+      <CategoryGrid />
+
+      <FeaturedProducts />
+
+      <CategoryProducts />
     </div>
   );
 }
