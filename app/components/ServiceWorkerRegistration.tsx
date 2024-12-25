acf374e85b35212144ce2a73ae5e8257516df962
Shah Md. Mahi <shahmdmahi24@gmail.com>
2024-12-26 03:25:52 +0600

    updates

--- app/components/ServiceWorkerRegistration.tsx
+++ app/components/ServiceWorkerRegistration.tsx
@@ -6,6 +6,19 @@ import { registerServiceWorker } from "../pwa";
 export default function ServiceWorkerRegistration() {
   useEffect(() => {
     registerServiceWorker();
+
+    console.log(
+      "%cStop!",
+      "color: #ff0000; font-size: 40px; font-weight: bold;"
+    );
+    console.log(
+      '%cThis is a browser feature intended for developers. If someone told you to copy-paste something here to enable a feature or "hack" an account on nrlit.store, it is a scam and will give them access to your account.',
+      "color: #ffffff; font-size: 20px;"
+    );
+    console.log(
+      "See %chttps://nrlit.store/security-warning",
+      "color: #00aaff; text-decoration: underline; font-size: 20px;"
+    );
   }, []);
 
   return null;
