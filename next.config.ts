aef9f45e6b7319971fbc96909f4839d12d6715ad
Shah Md. Mahi <shahmdmahi24@gmail.com>
2024-12-27 00:17:59 +0600

    updates

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "drive.google.com",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
     ],
   },
 };
