02b336fde53dfbf145af3231174d9f4ce34b6c4e
Shah Md. Mahi <shahmdmahi24@gmail.com>
2025-01-28 14:50:06 +0600

    updates

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,6 +7,7 @@ import { Suspense } from "react";
 import ClientOnly from "../components/ClientOnly";
 import ServiceWorkerRegistration from "../components/ServiceWorkerRegistration";
 import TawkMessenger from "@/components/tawk-messenger";
+import { GoogleTagManager } from '@next/third-parties/google';
 
 export default async function Home() {
   const tawkPropertyId = process.env.TAWK_PROPERTY_ID!;
@@ -22,6 +23,7 @@ export default async function Home() {
 
         <CategoryProducts />
         <ClientOnly>
+          <GoogleTagManager gtmId={process.env.GTM_ID! as string} />
           <TawkMessenger propertyID={tawkPropertyId} widgetID={tawkWidgetId} />
           <ServiceWorkerRegistration />
         </ClientOnly>
