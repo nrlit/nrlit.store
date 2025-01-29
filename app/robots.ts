049757fb4bd029653205ace43494b9a74da25da0
Shah Md. Mahi <shahmdmahi24@gmail.com>
2025-01-30 00:21:28 +0600

    updates

--- app/robots.ts
+++ app/robots.ts
@@ -15,7 +15,7 @@ export default function robots(): MetadataRoute.Robots {
           '/checkout',
           '/user-profile',
           '/payment',
-          '/*?*', // Disallow all URL parameters
+          // '/*?*', // Disallow all URL parameters
         ]
       },
     ],
