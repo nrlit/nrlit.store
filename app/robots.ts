9babe198d8cf94c0b51ba7ff3bca835955a8a56e
Shah Md. Mahi <shahmdmahi24@gmail.com>
2025-02-01 00:52:34 +0600

    updates

--- app/robots.ts
+++ app/robots.ts
@@ -1,26 +1,57 @@
-import { MetadataRoute } from 'next'
+import type { MetadataRoute } from "next";
 
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://www.nrlit-store.com'
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://www.nrlit.store";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
-        userAgent: '*',
-        allow: '/',
+        userAgent: "*",
+        allow: [
+          "/",
+          "/categories/*",
+          "/shop",
+          "/about",
+          "/contact",
+          "/products/*",
+          "/faq",
+          "/terms-of-service",
+          "//privacy-policy",
+          "/return-refunds",
+        ],
         disallow: [
-          '/admin',
-          '/api',
-          '/orders',
-          '/checkout',
-          '/user-profile',
-          '/payment',
-          // '/*?*', // Disallow all URL parameters
-        ]
+          "/admin*",
+          "/api/*",
+          "/orders*",
+          "/checkout*",
+          "/user-profile*",
+          "/payment*",
+          "/callback*",
+          "/auth/*",
+          "/dashboard*",
+          "/*?fbclid=*",
+          "/*?utm_*",
+          "/*.json",
+          "/private/*",
+          "/temp/*",
+          "/draft/*",
+        ],
+      },
+      {
+        userAgent: "Googlebot",
+        allow: [
+          "/*.js",
+          "/*.css",
+          "/*.png",
+          "/*.jpg",
+          "/*.gif",
+          "/*.svg",
+          "/*.ico",
+          "/manifest.json",
+        ],
       },
     ],
     sitemap: `${baseUrl}/sitemap.xml`,
     host: baseUrl,
-  }
+  };
 }
-
